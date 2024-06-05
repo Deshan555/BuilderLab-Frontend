d7a9e373d79129f9dafe2f0e969589beb88032f9
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-06-05 09:40:53 +0530

    feat: Update API module to include fetchFullChecklistByIdForPublish and convertChecklist functions

--- src/api/api-config.js
+++ src/api/api-config.js
@@ -1,7 +1,11 @@
 const baseDetails = {
     BASE_URL: 'https://onlyforms-backend.onrender.com/',  
-    CORE_SERVICE_URL: 'https://onlyforms-backend.onrender.com/',  // Added 'http://'
-    // CORE_SERVICE_URL: 'https://thaprobane-com.onrender.com/thaprobane/core/v01',  // Added 'http://'
+    CORE_SERVICE_URL: 'https://onlyforms-backend.onrender.com/',
+
+    // BASE_URL: 'http://localhost:3001/',  
+    // CORE_SERVICE_URL: 'http://localhost:3001/',
+    CONVERTER_SERVICE_URL: 'https://edotcodevapi.eimsky.com/pisService/',
+
     STORAGE_SERVICE_URL: 'https://api.themoviedb.org/3',
     GATEWAY_SERVICE_URL: 'https://api.themoviedb.org/3',
     ML_MODEL_SERVICE_URL: 'http://localhost:8000/', 
